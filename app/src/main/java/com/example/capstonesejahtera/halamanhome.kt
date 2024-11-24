994ca888ddafae1ee318983279a6b2e08894460b
MuhIrsyad <irsyadnaxcholy12@gmail.com>
2024-11-24 14:29:56 +0700

    dark mode tetap putih

--- app/src/main/java/com/example/capstonesejahtera/halamanhome.kt
+++ app/src/main/java/com/example/capstonesejahtera/halamanhome.kt
@@ -1,5 +1,6 @@
 package com.example.capstonesejahtera
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -7,5 +8,16 @@ class halamanhome : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.halamanhome)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+        // Navigasi kembali ke MainActivity
+        val intent = Intent(this, MainActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP or Intent.FLAG_ACTIVITY_NEW_TASK)
+        startActivity(intent)
+
+        // Tidak memanggil super.onBackPressed() untuk mencegah aksi default
+        // (Menutup aktivitas saat ini secara bawaan).
     }
 }
